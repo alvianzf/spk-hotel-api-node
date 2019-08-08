06f0c77a9e5a70428765b27a4d4430a3b97ebee0
Alvian Z Faturrahman <alvian.faturrahman@webimp.com.sg>
2019-08-09 00:23:30 +0700

    changes

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-mongoose.connect(db.url || db.staging, {
+mongoose.connect((db.url || db.staging), {
     useNewUrlParser: true
 }).then(function(docs) {
     console.log(db.url + ' connected')
