15e62406a1f3fcb5f03c1ca10ef3d331b95f3524
Alvian Z Faturrahman <alvian.faturrahman@webimp.com.sg>
2019-08-09 14:55:14 +0700

    change login to email

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,7 +2,7 @@ var router = require('express').Router()
 var user = require('mongoose').model('Users')
 
 router.get('/', function(req, res) {
-    user.findOne({name: req.body.name, password: req.body.password}, function(err, user) {
+    user.findOne({email: req.body.name, password: req.body.password}, function(err, user) {
         if (err) return res.status(422).json({status: 422, message: err.message});
 
         return res.status(200).json({status: 200, token: Date()});
